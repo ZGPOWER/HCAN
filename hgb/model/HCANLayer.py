9cb3890efc280ccd2ec66a457cf452f43fe1c943
ZGPOWER <zhangzq22@mails.tsinghua.edu.cn>
2025-03-19 16:42:29 +0800

    Update HCANLayer.py

--- hgb/model/HCANLayer.py
+++ hgb/model/HCANLayer.py
@@ -9,6 +9,7 @@ class HCANLayer(nn.Module):
                  use_norm=False, device='cpu', c_heads=1):
         super(HCANLayer, self).__init__()
         self.num_heads = num_heads
+        self.ntypes=ntypes
         self.nlayers = nlayers
         self.layers = nn.ModuleList()
         self.negative_slope = negative_slope
@@ -18,38 +19,65 @@ class HCANLayer(nn.Module):
         self.in_feat = in_feat
         self.hid_dim = hid_dim
         self.use_norm = use_norm
-
         if nlayers > 1:
             self.layers.append(
-                RelConv(in_channels=in_feat, out_channels=hid_dim, num_node_types=ntypes, num_edge_types=num_rels,
-                         use_norm=use_norm, c_heads=c_heads))
+                SGCConv(in_channels=hid_dim, out_channels=hid_dim, num_node_types=ntypes, num_edge_types=num_rels,
+                         use_norm=use_norm, c_heads=c_heads)
+            )
             for _ in range(1, nlayers - 1):
                 self.layers.append(
-                    RelConv(in_channels=hid_dim, out_channels=hid_dim, num_node_types=ntypes, num_edge_types=num_rels,
-                             use_norm=use_norm, c_heads=c_heads))
-            self.layers.append(CoA(in_channels=hid_dim, out_channels=hid_dim, num_node_types=ntypes,
+                    SGCConv(in_channels=hid_dim, out_channels=hid_dim, num_node_types=ntypes, num_edge_types=num_rels,
+                             use_norm=use_norm, c_heads=c_heads)
+                )
+            self.layers.append(MixConv(in_channels=hid_dim*self.nlayers, out_channels=hid_dim, num_node_types=ntypes,
                                        num_edge_types=num_rels, heads=num_heads, use_norm=use_norm, att_dropout=att_dropout, c_heads=c_heads))
         else:
-            self.layers.append(CoA(in_channels=in_feat, out_channels=hid_dim, num_node_types=ntypes,
+            self.layers.append(MixConv(in_channels=hid_dim, out_channels=hid_dim, num_node_types=ntypes,
                                        num_edge_types=num_rels, heads=num_heads, use_norm=use_norm, att_dropout=att_dropout, c_heads=c_heads))
+        self.feat_lins = nn.ModuleList([nn.Linear(hid_dim, hid_dim) for _ in range(ntypes)])
+        self.root_lins = nn.ModuleList([nn.Linear(in_feat, hid_dim) for _ in range(ntypes)])
         if self.use_norm:
-            self.norm = nn.LayerNorm(hid_dim)
+            self.norm_1 = nn.LayerNorm(hid_dim*self.nlayers)
+            self.norm_2 = nn.LayerNorm(hid_dim)
+        self.act = nn.LeakyReLU()
+        self.dp = nn.Dropout(p=self.dropout)
         self.reset_parameters()
 
     def reset_parameters(self):
+        gain = nn.init.calculate_gain('leaky_relu')
         for layer in self.layers:
             layer.reset_parameters()
+        for lin in self.feat_lins:
+            nn.init.xavier_normal_(lin.weight,gain=gain)
+            nn.init.zeros_(lin.bias)
+        for lin in self.root_lins:
+            nn.init.xavier_normal_(lin.weight,gain=gain)
+            nn.init.zeros_(lin.bias)
+
+    def proj_x(self, x, node_type, root=False):
+        outs = []
+        for i in range(self.ntypes):
+            if root:
+                outs.append(self.root_lins[i](x))
+            else:
+                outs.append(self.feat_lins[i](x))
+        outs = torch.stack(outs, dim=1)
+        node_type_expanded = node_type.unsqueeze(1).unsqueeze(2).expand(-1, 1, outs.size(2))
+        proj = outs.gather(dim=1, index=node_type_expanded).squeeze(1)
+        return proj
 
     def forward(self, x, edge_index, edge_attr, node_type):
-        for i, layer in enumerate(self.layers):
-            if x.device != self.device:
-                x = x.to(self.device)
-            out = layer(x, edge_index, edge_attr, node_type)
-            x = out
-            if self.use_norm:
-                x = self.norm(x)
-            x = F.dropout(x, p=self.dropout, training=self.training)
-            if i != len(self.layers) - 1:
-                x = F.leaky_relu(x, negative_slope=self.negative_slope)
+        x = self.proj_x(x, node_type, True)
+        tmp = [x]
+        for i in range(self.nlayers-1):
+            x = self.layers[i](x, edge_index, edge_attr, node_type)
+            tmp.append(self.proj_x(x, node_type))
+        x = torch.concat(tmp, dim=1)
+        if self.use_norm:
+            x = self.norm_1(x)
+        x = self.dp(x)
+        x = self.act(x)
+        x = self.layers[-1](x, edge_index, edge_attr, node_type)
+        if self.use_norm:
+            x = self.norm_2(x)
         return x
-
